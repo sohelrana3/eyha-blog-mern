f2ba874df6e0e02b9f1733ffe3a1fb8324528e3b
Sohel Rana <expartdesigns5@gmail.com>
2023-12-21 20:46:38 +0600

    all post

--- deshboard/src/pages/AllPost.jsx
+++ deshboard/src/pages/AllPost.jsx
@@ -1,12 +1,84 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import DeshboardTop from "../components/DeshboardTop";
+import axios from "axios";
+import baseUrl from "../utils/Content";
+import {
+    UserOutlined,
+    EyeOutlined,
+    EditOutlined,
+    DeleteOutlined,
+} from "@ant-design/icons";
+import { Col, Row, Card, Flex, Avatar, Button } from "antd";
 
 const AllPost = () => {
-  return (
-    <div className="deshboard">
-            <DeshboardTop deshtitle="All Post"/>
-        </div>
-  )
-}
+    let [data, setdata] = useState([]);
 
-export default AllPost
+    // useEffect create
+    useEffect(() => {
+        const getdata = async () => {
+            const res = await axios.get(`${baseUrl}/api/v1/auth/getpost`);
+            setdata(res.data);
+            console.log(res.data);
+        };
+        getdata();
+    }, []);
+    return (
+        <div className="deshboard">
+            <DeshboardTop deshtitle="All Post" />
+            <Row gutter={[32]} style={{ marginTop: "50px" }}>
+                <Col span={32}>
+                    <Card>
+                        <Flex justify="space-between">
+                            <div>
+                                <h2>Recent Posts</h2>
+                                <p>View and edit your latest posts</p>
+                            </div>
+                            <Button type="primary" size="default">
+                                View More
+                            </Button>
+                        </Flex>
+                        {/* map */}
+                        {data.map((item) => (
+                            <Flex style={{ paddingTop: "20px" }} gap="20px">
+                                <Flex style={{ width: "80%" }} gap="20px">
+                                    <Avatar
+                                        shape="square"
+                                        size={64}
+                                        icon={<UserOutlined />}
+                                    />
+                                    <div>
+                                        <Flex gap="10px">
+                                            <p>rezaiimohammad@gmail.com</p>
+                                            <p>October 25th, 2020 08:55 AM</p>
+                                        </Flex>
+
+                                        <h3>
+                                            Lorem ipsum dolor sit amet,
+                                            consectetur adipiscing elit,
+                                        </h3>
+                                        <p>
+                                            Lorem ipsum dolor sit amet,
+                                            consectetur adipiscing elit, sed do
+                                            eiusmod tempor incididunt ut labore
+                                            et dolore magna aliqua Lorem ipsum
+                                            dolor sit
+                                        </p>
+                                    </div>
+                                </Flex>
+                                <Flex gap="20px">
+                                    <DeleteOutlined style={{ color: "red" }} />
+                                    <EditOutlined style={{ color: "green" }} />
+                                    <UserOutlined
+                                        style={{ color: "#1565D8" }}
+                                    />
+                                </Flex>
+                            </Flex>
+                        ))}
+                    </Card>
+                </Col>
+            </Row>
+        </div>
+    );
+};
+
+export default AllPost;
--- deshboard/src/pages/CreatePost.jsx
+++ deshboard/src/pages/CreatePost.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import DeshboardTop from "../components/DeshboardTop";
+import { Col, Row, Card, Flex, Avatar, Button } from "antd";
 
 const CreatePost = () => {
     return (
         <div className="deshboard">
             <DeshboardTop deshtitle="Create Post" />
+
+            <Row gutter={[32]} style={{ marginTop: "50px" }}>
+                <Col span={32}>
+                    
+                </Col>
+            </Row>
         </div>
     );
 };
