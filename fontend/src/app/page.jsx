389a671a09bf8463f55b69f375b159a2a971950e
Sohel Rana <expartdesigns5@gmail.com>
2024-01-24 21:35:44 +0600

    cotract section

--- fontend/src/app/page.jsx
+++ fontend/src/app/page.jsx
@@ -1,12 +1,14 @@
 // inport components
 import Banner from "@/components/Banner";
 import Product from "@/components/Product";
+import Contract from "@/components/Contract";
 
 export default async function Home() {
     return (
         <>
             <Banner />
             <Product />
+            <Contract />
         </>
     );
 }
